5dd14cd8559b582528a0c0d94dc7561f09081b78
Carlton Brown <carltonbrown@github.com>
2023-11-14 13:41:23 -0500

    do not merge to main

---
+++ create_draft_pr.sh
@@ -0,0 +1,8 @@
+### Do not let this into main
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/carltonbrown/odaiba/pulls \
+  -d '{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"feature-9","base":"main","draft":"true"}'
